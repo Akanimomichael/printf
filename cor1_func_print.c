1b518cb90e32616ca8319b8b18306e7c5df9747a
Tocsmatts <toochukwuezeobi@gmail.com>
2022-07-12 13:33:28 -0700

    File function gives the amount of identifiers

---
+++ cor1_func_print.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * ev_print_func - returns the amount of identifiers.
+ * @c: This is a char argument indentifier
+ * @idx: This is the index of argument identifier.
+ * Return: amount of identifiers.
+ */
+int cor1_func_print(const char *c, int idx)
+{
+	func_point pr[] = {
+		{"c", print_c}, {"s", print_s}, {"i", print_id},
+		{"d", print_id}, {"%", print_other}, {"l", print_other},
+		{"h", print_other}, {NULL, NULL},
+	};
+	int x = 0, y = 0, fst_index;
+
+	fst_index = idx;
+	while (pr[x].type_arg)
+	{
+		if (c[idx] == pr[x].type_arg[y])
+		{
+			if (pr[x].type_arg[y + 1] != '\0')
+				idx++, y++;
+			else
+				break;
+		}
+		else
+		{
+			y = 0;
+			x++;
+			idx = fst_index;
+		}
+	}
+	return (y);
+}
